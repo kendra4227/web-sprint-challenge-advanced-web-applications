8652504f7baa54ed064cd5940237b0656aedc65d
Lakendria <kendra939@gmail.com>
2021-01-23 12:20:47 -0500

    built axiosWithAuth

--- client/src/axiosWithAuth.js
+++ client/src/axiosWithAuth.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import axios from 'axios';
 
-const axiosWithAuth = () =>{
-    
+export const axiosWithAuth = () =>{
+    const token = JSON.parse(localStorage.getItem("token"));
+    return axios.create({
+      headers: {
+        Authorization: `${token}`,
+      },
+      baseURL: "http://localhost:5000",
+    });
 }
     
 
 
-export default axiosWithAuth;
