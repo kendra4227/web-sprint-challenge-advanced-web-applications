7428d8a54d7295a1d0ad52591ef31c833d7beb23
Lakendria <kendra939@gmail.com>
2021-01-23 11:56:01 -0500

    built login form

---
+++ client/src/Login.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import axios from 'axios';
+import axiosWithAuth from './axiosWithAuth';
+
+state = {
+    credentials: {
+        username:"",
+        password:""
+    }
+}
+
+handleChange = e =>{
+    this.setState({
+        credentials:{
+            ...this.state.credentials,
+            [e.target.name]: e.target.value
+        }
+    });
+};
+
+export const Login = (e) =>{
+
+    e.preventDefault();
+    axios.post("http://localhost:5000/api/login", this.state.credentials)
+.then(res=> { 
+    console.log(res)
+    localStorage.setItem("token", res.data.payload); 
+})
+
+.catch(err => console.log(err));
+ 
+
+};
+    
+        render() {
+return(
+    <div className = "form">
+        <form onSubmit ={this.login}>
+            <input 
+            type="text" 
+            name="username" 
+            value={this.state.credentials.username}
+            onChange={this.handleChange}/>
+<input 
+            type="password" 
+            name="password" 
+            value={this.state.credentials.password}
+            onChange={this.handleChange}/>
+            <button>Log In</button>
+        </form>
+    </div>
+)
+       
+        
+        
+}
