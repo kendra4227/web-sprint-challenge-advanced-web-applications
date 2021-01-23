fb0e09123f60ec80635a0e3df38ba986740c4299
Lakendria <kendra939@gmail.com>
2021-01-23 12:29:42 -0500

    Private Route done

---
+++ client/src/components/PrivateRoute.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {Redirect, Route} from 'react-router-dom'
+
+const PrivateRoute = ({component :Component, ...rest}) =>{
+    return(
+        <Route
+        {...rest}
+         render ={(routeProps)=> {
+        // If the token is in the localStorage, render the 
+        if (localStorage.getItem("token")){
+            console.log("token found, rendering component");
+            return <Component {...routeProps}/>
+        }
+          //Otherwise redirect login
+        else {
+            console.log("no token found, redirect to login");
+            return <Redirect to = "/login"/>
+        }
+        }
+    }
+         />       
+    )
+}
+
+export default PrivateRoute;
