a85be971b951e89f80eccd83ed9615df2deca490
Lakendria <kendra939@gmail.com>
2021-01-24 12:06:17 -0500

    login working

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,71 +1,66 @@
 import React, {useState} from "react";
-import axios from "axios";
+import { useHistory } from 'react-router-dom';
+import {axiosWithAuth} from '../utils/axiosWithAuth';
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   
     
-   const state ={
-        credentials:{
+   const initialState ={
             username:"",
             password:""
         }
-}
-  //POST REQUEST 
-axios.post("http://localhost:5000/api/login",state.credentials)
-.then(res=> { 
-    console.log(res)
-    localStorage.setItem("token", res.data.payload); 
 
-    this.props.history.push('/colors')
-})
-.catch(err =>{
-  console.log(err);
-
-}) 
-const Login =()=> {
-  const [user, setUser] = useState(state);
+  
+const Login =(props)=> {
+  const history = useHistory()
+  const [credentials, setCredentials] = useState(initialState);
 
 const handleChange = (e) =>{
-  e.persist();
-  let value = e.target.value;
-    this.setState({
-        credentials:{
-            ...this.state.credentials,
-            [e.target.name]: e.target.value
-        }
-    });
-
-setUser({
-  ...user,
-  [e.target.name]:value
-});
+  setCredentials({
+    ...credentials,
+    [e.target.name] : e.target.value
+  });
 }
 
 const handleSubmit = (e) =>{
-  e.preventDefault();
+  e.preventDefault()
+  axiosWithAuth()
+  .post('/api/login', credentials)
+  .then(res => {
+    localStorage.setItem('token', res.data.payload);
+    history.push('/api/colors')
+  })
+  .catch(err => {
+    console.log('POST ERROR: ' + err)
+  })
+  setCredentials(credentials)
 }
 
+
 return(
-  
+  <>
+  <h1>Welcome to the Bubble App!</h1>
     <div className = "form">
         <form onSubmit ={handleSubmit}>
+          <label htmlFor="username">Username:</label>
             <input 
             type="text" 
             name="username" 
-            value={state.credentials.username}
+            value={credentials.username}
             onChange={handleChange}/>
-
-<input 
+            <label htmlFor="password">Password:</label>
+            <input 
             type="password" 
             name="password" 
-            value={state.credentials.password}
+            value={credentials.password}
             onChange={handleChange}/>
 
             <button>Log In</button>
         </form>
     </div>
+    </>
 );
-
-  }
+}
+  
   
 export default Login;
